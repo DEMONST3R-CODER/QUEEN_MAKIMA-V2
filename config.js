0e6b58d481e08f53cdab155c5235fe8aad3832cf
DEMONST3R-CODER <wisdomoseghale7@gmail.com>
2024-11-16 01:41:35 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+
+//
+global.available = true;
+global.autoReadAll = false;
+global.antitags = true;
+
+
+//auto functioner
+global.autoTyping = false;                //auto tying by default off.
+global.autoRecord = false;                //auto recording by default off.
+global.groupevent = false;                //This is the new variable for controlling group event handling.
+global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.autoreadgc = false;
+
+
+//
+global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+
+
+//
+global.Owner = ["2347079059033", "2349153596300"];         //If you want singal number so global.Owner = ['2347079059033'] Change into your number.
+global.OwnerNumber = ["2347079059033", "2349153596300"];   //If you want singal number so global.Owner = ['2347079059033'] Change into your number.
+global.ownertag = ["2347079059033"];
+global.OwnerName = "꧁𝕿𝖍𝖚𝖌𝖓𝖎𝖋𝖎𝖈𝖊𝖓𝖙ᵈᵒᵒᵐ 𒆜𓊉꧂ ☠️";
+global.BotName = "QUEEN MAKIMA_V2";
+global.packname = "🦋 𝕼𝖚𝖊𝖊𝖓 𝖒𝖆𝖐𝖎𝖒𝖆 ❦︎ 🦋";                             //Do not change.
+global.author = "꧁𝕿𝖍𝖚𝖌𝖓𝖎𝖋𝖎𝖈𝖊𝖓𝖙ᵈᵒᵒᵐ 𒆜𓊉꧂ ☠️ ";                               //Do not change.
+global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
+global.SupportGroupLink = "https://whatsapp.com/channel/0029VaoOiuwDp2QH070eTE01";
+
+
+//
+global.sessionName = "session";                          //Do not change.
+
+
+//
+global.openAiAPI = "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";
+
+
+//
+global.location = "Delta state, Nigeria";
+global.reactmoji = "🤓";
+global.themeemoji = "😂";
+global.vidmenu = { url: 'https://files.fm/u/w3urkbskms' };
+global.websitex = "https://github.com/DEMONST3R-CODER/QUEEN_MAKIMA-V2";
+global.lolhuman = "꧁𝕿𝖍𝖚𝖌𝖓𝖎𝖋𝖎𝖈𝖊𝖓𝖙ᵈᵒᵒᵐ 𒆜𓊉꧂ ☠️";
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+  jobdone: '*DONE 🤭*',
+  useradmin: '*ADMINS ONLY*!',
+  botadmin: '*ONLY ADMINS HAVE ACCESS TO THIS COMMAND*.',
+  botowner: '"YOU ARE NOT MY OWNER, GAY!*',
+  grouponly: '*GROUP CHATS ONLY*',
+  privateonly: '*PRIVATE CHAT ONLY 🌝*!',
+  botonly: '*BOT ONLY!*',
+  waiting: '*PROCESSSING* 👨‍💻.....',
+  nolink: '*UGH 😩, KINDLY PROVIDE A LINK!*',
+  error: '*ERROR* :(',
+  banned: '$YOU ARE BANNED FROM USING THIS COMMAND*!',
+  bangc: 'THIS GROUP IS *BANNED* FROM USING THIS COMMAND!',
+  nonsfw: '*PERVERT*😔!'
+
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
